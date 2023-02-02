26a4afed6c301676dbfe951cbf63cf0f2cc2bbee
blake S <design.harls@icloud.com>
2023-02-02 15:33:46 +0100

    added photo form

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 // import router from './router'
 
 
-// import './sass/styles.scss'
+import './sass/styles.scss'
 const app = createApp(App)
 
 app.use(createPinia())
--- src/main/frontend/src/views/FormNewAnimal.vue
+++ src/main/frontend/src/views/FormNewAnimal.vue
@@ -1,41 +1,54 @@
 <script setup></script>
 <template>
-  <div class="wrapper">
-    <div class="wrapper-side">
-      <img src="" alt="" />
-    </div>
-    <div>
+ 
+    
+  
+
       <form class="form">
+        <div class="__form-side">
+           <img src="../assets/img/PhotoForm.jpg" alt="Photo-form">
+        </div>
+        <div class="__form-container">
+
+        
         <h1 class="title">FORM NEW ANIMAL</h1>
 
         <div class="__form-group">
-          <label for="name" class="__container-forms__label">Name</label>
+          <label for="name" class="__container-forms__label" placeholder="">Name</label>
           <input type="text" class="__container-forms__label__inputbox" name="name"/>
+          <span class="form__line"></span>
         </div>
 
         <div class="__form-group">
-          <label for="type" class="__container-forms__label">Type</label>
+          <label for="type" class="__container-forms__label" placeholder=" ">Type</label>
           <input type="text" class="__container-forms__label_inputbox" name="type"/>
+          <span class="form__line"></span>
         </div>
 
         <div class="__form-group">
-          <label for="family" class="__container-forms__label">Family</label>
+          <label for="family" class="__container-forms__label" placeholder="">Family</label>
           <input type="text" class="__container-forms__label_inputbox" name="family"/>
+          <span class="form__line"></span>
         </div>
 
         <div class="__form-group">
-          <label for="gender" class="__container-forms__label">Gender</label>
+          <label for="gender" class="__container-forms__label" placeholder="">Gender</label>
             <input type="text" class="__container-forms__label_inputbox"  name="gender"/>
+            <span class="form__line"></span>
         </div>
          
 
-        <div>
-          <label for="date" class="__container-forms__label">Date</label>
-          <input type="text"   class="__container-forms__label_inputbox" name="date" />
+        <div  class="__form-group">
+          <label for="date" class="__container-forms__label" placeholder="">Date</label>
+          <input type="text" class="__container-forms__label_inputbox" name="date" />
+          <span class="form__line"></span>
         </div>
       
-    </form>
+        <button type="submit" class="btn__form" value="SEND">SEND</button>
+
+        
     </div>
-  </div>
+    </form>
+
 </template>
 <style lang="scss" scoped></style>
