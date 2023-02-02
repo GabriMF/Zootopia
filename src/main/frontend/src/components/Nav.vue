a7107686fb0276880af63a868a350cd25f8563c8
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-02-02 21:26:10 +0100

    configuracion

---
+++ src/main/frontend/src/components/Nav.vue
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<template>
+    <div id = "nav">
+
+       <input type="text">
+      
+    </div>
+</template>
+
+<style lang = "scss" scoped>
+
+#nav {
+    height: 100%;
+    width: 25%;
+    border-radius: 10px;
+    background-color: #FDBBAC;
+}
+
+input {
+    width: 80%;
+    background-color: black;
+}
+</style>
