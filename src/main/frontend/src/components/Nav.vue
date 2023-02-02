8c9bb2313bd49c8c951fb4dbc15c302071032b61
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-02-03 00:22:34 +0100

    en proceso

--- src/main/frontend/src/components/Nav.vue
+++ src/main/frontend/src/components/Nav.vue
@@ -2,24 +2,42 @@
 </script>
 
 <template>
-    <div id = "nav">
-
-       <input type="text">
-      
+    <div id="nav">
+        <div>
+            <p>FILTER FAMILY<input type="text"></p>
+            <p>FILTER COUNTRY<input type="text"></p>
+        </div>
+        <div>
+            <button>HOME</button>
+            <button>ADD +</button>
+        </div>
     </div>
 </template>
-
+ 
 <style lang = "scss" scoped>
 
 #nav {
-    height: 100%;
+    height: 100vh;
     width: 25%;
-    border-radius: 10px;
     background-color: #FDBBAC;
+}
+
+p {
+    padding: 25px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 
 input {
     width: 80%;
-    background-color: black;
+    background-color: white;
+    border-radius: 10px;
+    margin: 40px;
+
+}
+
+button {
+    border-radius: 10px;
+    margin: 20px;
+    // border: red 2px solid;
 }
 </style>
