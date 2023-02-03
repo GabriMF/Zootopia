0d7b1d3e9fc26b3f8218da6dc3046e5f21b8290a
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-02-03 12:16:40 +0100

    nav finalizado

--- src/main/frontend/src/components/Nav.vue
+++ src/main/frontend/src/components/Nav.vue
@@ -3,13 +3,28 @@
 
 <template>
     <div id="nav">
-        <div>
-            <p>FILTER FAMILY<input type="text"></p>
-            <p>FILTER COUNTRY<input type="text"></p>
-        </div>
-        <div>
-            <button>HOME</button>
-            <button>ADD +</button>
+       
+            <select class="filterFamily" name="FILTER FAMILY" required>
+                <option value="" disabled selected hidden>FILTER FAMILY</option>
+                <option value="felidos">Felidos</option>
+                <option value="leporidae">Leporidae</option>
+                <option value="reptiles">Reptiles</option>
+                <option value="mustelids">Mustelids</option>
+                <option value="canidos">Canidos</option>
+            </select>
+     
+            <select class="filterCountry" name="FILTER COUNTRY" required>
+                <option value="" disabled selected hidden>FILTER COUNTRY</option>
+                <option value="angola">Angola</option>
+                <option value="españa">España</option>
+                <option value="australia">Australia</option>
+                <option value="kenia">Kenia</option>
+                <option value="ghana">Ghana</option>
+            </select>
+
+        <div class="conteinerButton">
+            <button><i class="fa-solid fa-house homeButtonImg"></i>HOME</button>
+            <button><i class="fa-solid fa-user-plus addButtonImg"></i>ADD +</button>
         </div>
     </div>
 </template>
@@ -18,26 +33,40 @@
 
 #nav {
     height: 100vh;
-    width: 25%;
+    width: 15%;
     background-color: #FDBBAC;
 }
 
-p {
-    padding: 25px;
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-}
-
-input {
-    width: 80%;
-    background-color: white;
-    border-radius: 10px;
-    margin: 40px;
+.filterFamily {
+    padding: 15px;
+    border-bottom: 1px solid black;
 
 }
+
+.filterCountry {
+    padding: 15px;
+    border-bottom: 1px solid black;
+}
+
 
 button {
     border-radius: 10px;
     margin: 20px;
-    // border: red 2px solid;
+    
+}
+.conteinerButton {
+
+margin-top: 15em;
+
+}
+
+.homeButtonImg {
+    margin-right: 1em;
+  
+}
+
+.addButtonImg {
+    margin-right: 1em;
+    
 }
 </style>
