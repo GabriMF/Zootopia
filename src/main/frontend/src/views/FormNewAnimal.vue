9de2f960686a743a981f17589a63dd7427a42f14
blake S <design.harls@icloud.com>
2023-02-05 14:16:07 +0100

    added vee-validate

--- src/main/frontend/src/views/FormNewAnimal.vue
+++ src/main/frontend/src/views/FormNewAnimal.vue
@@ -1,181 +1,121 @@
-<script setup></script>
+<script setup>
+import {Field , Form,ErrorMessage} from 'vee-validate';
+// export default{
+//   components:{
+//     Form,
+//     Field,
+//   },
+  // methods:{
+   function onSubmit(values){
+      console.log(values,null,2);
+        }
+   function validateForm(value){
+    //si el campo esta vacio 
+    if(!value){
+      return ' This field is required';
+    }
+    return true;
+   }
+
+
+</script>
+
+
+
 <template>
-  <main>
-    
-    
-    <div class="side">
-       <img src="../assets/img/PhotoForm.jpg"  width="100%" height="100%" class="__side-img" alt="Photo-form">
+  <div class="main">
+    <Form @submit="onSubmit" class="form__wrapper">
+      <div class="form__img">
+        <img src="../assets/img/img-form.jpg" alt="photo-form" />
       </div>
-    <form class="forms">
-      
-
-
-          
-            <h1 class="forms__title">FORM NEW ANIMAL</h1>
-            <div class="forms__group">
-              <input type="text" class="forms__inputbox" placeholder="Name" name="name"/>
-              <span class="forms__line"></span>
-            </div>
-            <div class="forms__group">
-              <input type="text" class="forms__inputbox" placeholder="Type" name="name"/>
-              <span class="forms__line"></span>
-            </div>
-              
-              
-    
-            
-            <div class="forms__group">
-              <input type="text" class="forms__inputbox" placeholder="Family" name="name"/>
-              <span class="forms__line"></span>
-            </div>
-            <div class="forms__group">
-              <input type="text" class="forms__inputbox" placeholder="Gender" name="name"/>
-                <span class="forms__line"></span>
-            </div>
-            <div  class="forms__group">
-              <input type="text" class="forms__inputbox" placeholder="Date" name="name"/>
-              <span class="forms__line"></span>
-            </div>
-            
-            <button type="submit" class="forms__btn" value="send">SEND</button>
-          
-       
-    </form>
-
-
-
-
-          
-  
-  
-           
-  
-        
-  
-
-    </main>
+      <div class="form__container">
+        <div class="form__title">
+          <h1>NEW FORM ANIMAL</h1>
+        </div>
+        <div class="form__fields">
+          <div class="form__group">
+            <Field type="text" class="form__input" placeholder="Name" name="name" :rules="validateForm"/>
+            <ErrorMessage name="name" />
+            <span class="form__line"></span>
+          </div>
+          <div class="form__group">
+            <Field type="text" class="form__input" placeholder="Type" name="type"/>
+            <span class="form__line"></span>
+          </div>
+          <div class="form__group">
+            <Field type="text" class="form__input" placeholder="Family" name="family"/>
+            <span class="form__line"></span>
+          </div>
+          <div class="form__group">
+            <Field type="text" class="form__input" placeholder="Gender" name="gender"/>
+            <span class="form__line"></span>
+          </div>
+          <div class="form__group">
+            <Field type="text" class="form__input" placeholder="Date" name="Date"/>
+            <span class="form__line"></span>
+          </div>
+        </div>
+        <button type="submit" class="form_btn" value="send">SEND</button>
+      </div>
+    </Form>
+  </div>
 </template>
+
 <style lang="scss" scoped>
 @use "@/sass/mixins" as m;
- 
 
-
-main{
- 
-  // @include m.flex(flex,row,auto,center,center);
-  width: 60%;
-  height: 40%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-
-  margin: 0 auto;
-  .side{
-  width:100%;
+.main{
+  @include m.flex(grid,auto,auto,center,center);
+  width: 100%;
   height: 100%;
-  background-color: red;
-  
-}
+  // grid-template-columns: 1fr 1fr;
+  background-color: blue;
 
-  .forms{
-    height:50vh;
-    width: 50%;
-    padding: 9em;
+  .form__wrapper{
     display: flex;
-    flex-direction: column;
-    background-color: pink;
-   
-   
+    background-color: red;
+    justify-content: center;
+    align-items: flex-start;
+    border-style: solid;
+    .form__img{
+      background-color: bisque;
 
-    &__title{
-      font-size:25px;
-      margin-bottom: .5em;
-      margin-top:3em;      
-    }
-
-    &__group{
-    position: relative;
-    }
-     
-    &__inputbox{
-      
-      background: transparent;
-      width:100%;
-      color: black;
-      font-size: 1.1em;
-      padding :.8em 3em;
-      border:none;
-      margin-right: 0;
-      outline:none;
-      border-bottom: 1px solid #5757577e;
-      font-family: 'Courier New', Courier, monospace;
-    }
-      
-    &__btn{
-      background: rgb(238, 90, 90);
-      border: none;
-      font-weight: 300;
-      font-size: 1.4rem;
-      padding: .2em 0;
-      margin:2em;
-
-            
-    }
     
-&__line{
-position: absolute;
-bottom: 0;
-left: 0;
-width: 100%;
-height: 1px;
-transform: scale(0);
-transform: left bottom;
+    }
+
+
+    .form__container{
+      background-color: aqua;
+      display: flex;
+      flex-direction: column;
+      gap: 4px;
+     
+      
+      .form__title{
+        font-size: 12px;
+        display: flex;
+        justify-content: flex-end;
+
+      }
+
+    }
+
+
+  }
+  
+
+
+  
+
+
+
+
 
 }
-} 
-
-}
-    
-
- 
-     
-     
-       
-        
-      
-      
-            
-            
-    
-            
-            
-            
-                
-              
-          
-      
-      
-           
-          
-   
-  
-  
 
 
-     
 
-     
-  
+
+
+
 </style>
-
-
-
-
-   
-
-
-
-
-
-
-
-
