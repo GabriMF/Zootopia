80e0d54e3a2a9bc82b5e44f320def3118c3108d7
blake S <design.harls@icloud.com>
2023-02-09 00:33:57 +0100

    finished post test

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -2,12 +2,14 @@
 // import { RouterLink, RouterView } from 'vue-router'
 // import LandingView from './views/LandingView.vue';
 import DashBoardView from './views/DashBoardView.vue';
+import FormNewAnimal from './views/FormNewAnimal.vue';
 // import AnimalCard from '../src/components/AnimalCard.vue';
 </script>
 
 <template>
 <!-- <RouterView /> -->
 <DashBoardView />
+<FormNewAnimal/>
 <!-- <LandingView/> -->
 <!-- <AnimalCard/> -->
 </template>
